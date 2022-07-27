3b43eda166156a44f33ee247221e216640606804
Jeklah <arthurbowers@gmail.com>
2022-07-27 10:40:30 +0100

    Adding files for match_example

---
+++ match_example/src/main.rs
@@ -0,0 +1,46 @@
+enum Coin {
+    Penny
+    ,Nickel
+    ,Dime
+    ,Quarter(UsState)
+}
+
+#[derive(Debug)] //so we can inspect the state
+enum UsState {
+    Alabama
+    ,Alaska
+    ,Colorado
+    ,Texas
+    ,NewVegas
+    ,Washington
+    ,California
+}
+
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!!");
+            1
+        } 
+        ,Coin::Nickel => 5
+        ,Coin::Dime => 10
+        ,Coin::Quarter(state) => {
+            println!("state quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
