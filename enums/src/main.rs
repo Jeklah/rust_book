9a65b4285764cacff8fb128a663969d59d5a846b
Jeklah <arthurbowers@gmail.com>
2022-07-27 08:59:33 +0100

    Adding content about enums.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,48 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+
 fn main() {
-    println!("Hello, world!");
+   let four = IpAddrKind::V4;
+   let six = IpAddrKind::V6;
+   // let home = IpAddr {
+   //     kind: IpAddrKind::V4,
+   //     address: String::from("127.0.0.1"),
+   // };
+
+   // let loopback = IpAddr {
+   //     kind: IpAddrKind::V6,
+   //     address: String::from("::1"),
+   // };
+   // let home = IpAddr::V4(String::from("127.0.0.1"));
+
+   // let loopback = IpAddr::V6(String::from("::1"));
+
+   let home = IpAddr::V4(127, 0, 0, 1);
+
+   let loopback = IpAddr::V6(String::from("::1"));
+
+   fn route(ip_kind: IpAddrKind) {
+
+   }
+
+   route(IpAddrKind::V4);
+   route(IpAddrKind::V6);
 }
