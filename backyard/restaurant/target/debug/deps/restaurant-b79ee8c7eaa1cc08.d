09d89eee87408e7e2eb7189f55c89dc14eaab16d
Jeklah <arthurbowers@gmail.com>
2022-07-27 12:27:06 +0100

    Adding target to gitignore for restaurant modules.

--- backyard/restaurant/target/debug/deps/restaurant-b79ee8c7eaa1cc08.d
+++
@@ -1,7 +0,0 @@
-/home/kali/projects/rust/backyard/restaurant/target/debug/deps/restaurant-b79ee8c7eaa1cc08.rmeta: src/lib.rs
-
-/home/kali/projects/rust/backyard/restaurant/target/debug/deps/restaurant-b79ee8c7eaa1cc08.d: src/lib.rs
-
-src/lib.rs:
-
-# env-dep:CLIPPY_ARGS=
