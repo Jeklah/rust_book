a8d9ff9bb5969a52b13fa0a8addc28cc91fda13e
Jeklah <arthurbowers@gmail.com>
2022-07-27 12:25:09 +0100

    Adding restaurant example of scoping

---
+++ backyard/restaurant/target/debug/deps/restaurant-2fbe270a3510742e.d
@@ -0,0 +1,7 @@
+/home/kali/projects/rust/backyard/restaurant/target/debug/deps/restaurant-2fbe270a3510742e.rmeta: src/lib.rs
+
+/home/kali/projects/rust/backyard/restaurant/target/debug/deps/restaurant-2fbe270a3510742e.d: src/lib.rs
+
+src/lib.rs:
+
+# env-dep:CLIPPY_ARGS=
