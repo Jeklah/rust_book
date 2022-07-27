0790f21def7a433ce7d58edc9638c3a3552650f0
Jeklah <arthurbowers@gmail.com>
2022-07-27 10:59:34 +0100

    Adding content for concise control

--- concise_cont_flow/src/main.rs
+++ concise_cont_flow/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("the maximum is configured to be {}", max),
+        _ => (),
+    }
+    // this can be written shorter way as such:
+
+    let config2 = Some(3u8);
+    if let Some(max) = config2 {
+        println!("the maximum is configured to be {}", max);
+    }
+
+    // if else can include an else block. see matches example for more
+    // detail + code.
 }
