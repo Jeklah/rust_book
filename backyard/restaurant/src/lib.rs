1cc1f19240f629f9a41bb881c33af1d2ba52cd10
Jeklah <arthurbowers@gmail.com>
2022-07-28 19:04:26 +0100

    Updating lib.rs, although not 100% sure...

--- backyard/restaurant/src/lib.rs
+++ backyard/restaurant/src/lib.rs
@@ -1,18 +1,28 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
+pub use crate::front_of_house::hosting;
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
+
+
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//  
+//         fn seat_at_table() {}
+//     }
+//  
+//     mod serving {
+//        fn take_order() {}
+// 
+//        fn serve_order() {}
+// 
+//        fn take_payment() {}
+//     }
+// }
 
 fn deliver_order() {}
 
@@ -28,7 +38,7 @@ mod back_of_house {
     }
 
     impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
+         pub fn summer(toast: &str) -> Breakfast {
             Breakfast {
                 toast: String::from(toast),
                 seasonal_fruit: String::from("peaches"),
@@ -45,31 +55,31 @@ mod back_of_house {
     fn cook_order() {}
 }
 
-use crate::front_of_house::hosting;
 
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+// 
+//     // Relative path 
+//     front_of_house::hosting::add_to_waitlist();
+//     
+//     // Can do this due to the use crate above.
+//     hosting::add_to_waitlist();
+// 
+//     // Order a breakfast in the summer with Rye toast 
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     // Change our mind about wheet bread we'd like 
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+// 
+//     let order1 = back_of_house::Appetizer::Soup;
+//     let order2 = back_of_house::Appetizer::Salad;
+//     
+//     // the next line won't compile if we uncomment it; we're not allowed
+//     // to see or modify the seasonal_fruit that comes with the meal 
+//     // meal.seasonal_fruit = String::from("blueberries");
+// }
 
-    // Relative path 
-    front_of_house::hosting::add_to_waitlist();
-    
-    // Can do this due to the use crate above.
-    hosting::add_to_waitlist();
-
-    // Order a breakfast in the summer with Rye toast 
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // Change our mind about wheet bread we'd like 
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
-
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
-    
-    // the next line won't compile if we uncomment it; we're not allowed
-    // to see or modify the seasonal_fruit that comes with the meal 
-    // meal.seasonal_fruit = String::from("blueberries");
-}
 
 // mod customer {
 //     pub fn eat_at_restaurant() {
