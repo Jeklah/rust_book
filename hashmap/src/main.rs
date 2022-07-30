6e178ccfe38d5937345f6eefe8c2410f859f4c11
Jeklah <arthurbowers@gmail.com>
2022-07-30 11:51:22 +0100

    Adding files for hashmap

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -1,16 +1,39 @@
 use std::collections::HashMap;
-use std::fmt::Result;
-use std::io::Result as IoResult;
-
-fn function1() -> Result {
-    Ok(())
-}
-
-fn function2() -> IoResult<()> {
-    Ok(())
-}
 
 fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    let initial_scores = vec![10, 50];
+
+    // let mut scores: HashMap<_, _> = 
+    //     teams.into_iter().zip(initial_scores.into_iter()).collect();
+    let field_name = String::from("Favorite colour");
+    let field_value = String::from("Blue");
+
     let mut map = HashMap::new();
-    map.insert(1, 2);
+    map.insert(field_name, field_value);
+
+    scores.insert(String::from("Blue"), 10);
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
