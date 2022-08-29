ae9837a0258562bf0eea387633b442f1233f213c
Jeklah <arthurbowers@gmail.com>
2022-08-29 20:42:54 +0100

    Updating generics file as the rust book advances

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,13 +1,81 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
 
-    let mut largest = number_list[0];
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+// 
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
-    println!("The largest number is {}", largest);
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+// 
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+// 
+//     largest
+// }
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&number_list);
+    println!("the largest char is {}", result);
+
+    // let p = Point { x: 5, y: 10 };
+    // println!("p.x = {}", p.x());
+    
+    let p1 = Point { x:5, y: 10.4 };
+    let p2 = Point { x: "hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
