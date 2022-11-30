0873cf78379e88b3ce981f4fd23921e3a07696f2
ArthurB <arthur.bowers@phabrix.com>
2022-11-30 15:56:21 +0000

    Adding restaurant dir

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,39 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order () {}
+
+        fn serve_order () {}
+
+        fn take_payment() {}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
