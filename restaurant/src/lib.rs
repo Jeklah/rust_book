5be5daa7d39fcc4c11ecf6d00f0b59196c425ba4
Jeklah <arthurbowers@gmail.com>
2022-12-13 18:18:12 +0000

    Updating resturant lib.rs and creating real_restaurant

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,12 @@
+use std::fmt;
+use std::io::Result as IoResult;
+
+fn function() -> fmt::Result {}
+
+fn function2() -> IoResult<()> {}
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -13,6 +19,8 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
+
+use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
