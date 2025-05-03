7a1cdeaa4c6d1b14a326c967dabccad9d20edc06
Jeklah <arthurbowers@gmail.com>
2025-05-03 13:29:22 +0100

    correcting enum example.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -18,6 +18,21 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move{x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+struct QuitMessage; //unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String);  // tuple struct
+struct ChangeColorMessage(i32, i32, i32);
+
 
 fn main() {
    let four = IpAddrKind::V4;
@@ -45,4 +60,13 @@ fn main() {
 
    route(IpAddrKind::V4);
    route(IpAddrKind::V6);
+
+   impl Message {
+        fn call(&self) {
+            
+        }
+   }
+   
+   let m = Message::write!(String::from("hello"));
+   m.call(); 
 }
