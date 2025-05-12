ec7000156323bf410441d6ad61b533786af39189
Arthur Bowers <arthur.bowers@phabrix.com>
2025-05-12 15:23:50 +0100

    Adding messages directory in enums.

---
+++ enums/messages/src/main.rs
@@ -0,0 +1,30 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+impl Message {
+    fn call(&self) {
+        match self {
+            Message::Quit => println!("Quit"),
+            Message::Move { x, y } => println!("Move to x: {}, y: {}", x, y),
+            Message::Write(text) => println!("Write: {}", text),
+            Message::ChangeColor(r, g, b) => {
+                println!("Change color to r: {}, g: {}, b: {}", r, g, b)
+            }
+        }
+    }
+}
+
+// let m = Message::Write(String::from("hello"));
+// m.call();
