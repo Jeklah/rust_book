020a1a3a21c0787dda2fe29606a28dfc1dd23a24
Arthur Bowers <arthur.bowers@phabrix.com>
2025-06-09 16:30:39 +0100

    Fixing clippy optimisations.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -20,9 +20,9 @@ enum IpAddr {
 
 enum Message {
     Quit,
-    Move{x: i32, y: i32},
+    Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32)
+    ChangeColor(i32, i32, i32),
 }
 
 struct QuitMessage; //unit struct
@@ -30,43 +30,37 @@ struct MoveMessage {
     x: i32,
     y: i32,
 }
-struct WriteMessage(String);  // tuple struct
+struct WriteMessage(String); // tuple struct
 struct ChangeColorMessage(i32, i32, i32);
 
-
+impl Message {
+    fn call(&self) {}
+}
 fn main() {
-   let four = IpAddrKind::V4;
-   let six = IpAddrKind::V6;
-   // let home = IpAddr {
-   //     kind: IpAddrKind::V4,
-   //     address: String::from("127.0.0.1"),
-   // };
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
 
-   // let loopback = IpAddr {
-   //     kind: IpAddrKind::V6,
-   //     address: String::from("::1"),
-   // };
-   // let home = IpAddr::V4(String::from("127.0.0.1"));
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+    // let home = IpAddr::V4(String::from("127.0.0.1"));
 
-   // let loopback = IpAddr::V6(String::from("::1"));
+    // let loopback = IpAddr::V6(String::from("::1"));
 
-   let home = IpAddr::V4(127, 0, 0, 1);
+    let home = IpAddr::V4(127, 0, 0, 1);
 
-   let loopback = IpAddr::V6(String::from("::1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 
-   fn route(ip_kind: IpAddrKind) {
+    fn route(ip_kind: IpAddrKind) {}
 
-   }
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 
-   route(IpAddrKind::V4);
-   route(IpAddrKind::V6);
-
-   impl Message {
-        fn call(&self) {
-            
-        }
-   }
-   
-   let m = Message::write!(String::from("hello"));
-   m.call(); 
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
