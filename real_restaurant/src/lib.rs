d43883fccf23453c80c0cdebd45ee7f1922048d6
Jeklah <arthurbowers@gmail.com>
2025-06-23 16:25:21 +0100

    updating lib.rs

--- real_restaurant/src/lib.rs
+++ real_restaurant/src/lib.rs
@@ -1,7 +1,62 @@
-mod front_of_house;
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
-pub use crate::front_of_house::hosting;
+        fn sear_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
 
 pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind abour what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative term
+    front_of_house::hosting::add_to_waitlist();
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
