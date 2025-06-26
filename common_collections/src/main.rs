8c72083a8fa644eac500cfd2aadfa02dfc7c8f39
Jeklah <arthurbowers@gmail.com>
2025-06-26 20:12:17 +0100

    Updating common_collections

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,9 +1,27 @@
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
     let mut v = vec![1, 2, 3, 4, 5];
 
-    v.push(6);
-    v.push(7);
-    v.push(8);
-    v.push(9);
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
+    //let does_not_exist = &v[100];
+    let doesnt_exist = v.get(100);
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
