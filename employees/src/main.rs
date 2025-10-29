22299fcf278df4542397c706c5df75b546212ac2
Arthur Bowers <arthur.bowers@phabrix.com>
2025-10-29 16:30:20 +0000

    Reformatting comment so they all fit reasonably within a line.

--- employees/src/main.rs
+++ employees/src/main.rs
@@ -1,5 +1,6 @@
 // using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company.
-// For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
+// For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a
+// department or all people in the company by department, sorted alphabetically.
 // Bonus: Alphabetize the entries in the hash map by department.
 
 use std::collections::HashMap;
