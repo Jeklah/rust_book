6cc267eaed4654cb4e3928b97f0c5d89bb70485d
Arthur Bowers <arthurbowers@gmail.com>
2026-02-14 15:34:03 +0000

    Updating panic testing code.

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,19 +1,21 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, Read};
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
-
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {e:?}"),
-            },
-            _ => {
-                panic!("Problem opening the file: {error:?}");
-            }
-        },
-    };
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let username_file_result = File::open("hello.txt");
+    
+        let mut username_file = match username_file_result {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+    
+        let mut username = String::new();
+    
+        match username_file.read_to_string(&mut username) {
+            Ok(_) => Ok(username),
+            Err(e) => Err(e),
+        }
+    }
 }
+
