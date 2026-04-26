f83de2386da61bf1b093e3d2f695b3867e2769e9
Arthur Bowers <arthurbowers@gmail.com>
2026-04-26 12:48:40 +0100

    Adding more tests for adder.

--- 11.0_automated_tests/adder/src/lib.rs
+++ 11.0_automated_tests/adder/src/lib.rs
@@ -6,24 +6,58 @@ pub fn add_two(a: usize) -> usize {
     a + 2
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn exploration() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
     // #[test]
-    // fn another() {
-    //     panic!("Make this test fail");
+    // fn exploration() {
+    //     let result = add(2, 2);
+    //     assert_ne!(result, 4, "This test will fail because 2 + 2 equals 4");
     // }
 
     #[test]
     fn it_adds_two() {
         let result = add_two(2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    //#[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    #[should_panic]
+    fn another() {
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
