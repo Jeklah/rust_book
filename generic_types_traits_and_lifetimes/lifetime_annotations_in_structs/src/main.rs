306e092967131f526ec07e4656844fcb78d5ea55
Arthur Bowers <arthurbowers@gmail.com>
2026-04-26 10:50:43 +0100

    Adding lifetimes in structs dir and contents.

---
+++ generic_types_traits_and_lifetimes/lifetime_annotations_in_structs/src/main.rs
@@ -0,0 +1,23 @@
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {announcement}");
+        self.part
+    }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    let s: &'static str = "I have a static lifetime.";
+}
